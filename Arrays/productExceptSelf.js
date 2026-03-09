a01263d5a054d8f8e47ade911eb6067761cdc4d1
saketgoswami09 <saketgirigoswami4141@gmail.com>
2026-03-09 23:58:07 +0530

    max profit

--- Arrays/productExceptSelf.js
+++ Arrays/productExceptSelf.js
@@ -1,33 +1,45 @@
+// function productExceptSelf(nums) {
+//   const n = nums.length;
+//   const answer = new Array(n).fill(1);
+//   console.log(answer);
+
+//   let prefix = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     answer[i] = prefix;
+//     prefix *= nums[i];
+//   }
+// [1,2,3,4]
+//  console.log(nums);
+
+//   let suffix = 1;
+
+//   for (let i = n - 1; i >= 0; i--) {
+//     answer[i] *= suffix;
+//     suffix *= nums[i];
+//   }
+
+//   return answer;
+// }
+
+// console.log(productExceptSelf([1, 2, 3, 4]));
+
 function productExceptSelf(nums) {
-  const n = nums.length;
-  const answer = new Array(n).fill(1);
-  console.log(answer);
-  
+  let n = nums.length;
   let prefix = 1;
+  let ans = new Array(n).fill(1);
+  console.log(ans);
 
   for (let i = 0; i < n; i++) {
-    answer[i] = prefix;
+    ans[i] = prefix;
     prefix *= nums[i];
   }
-[1,2,3,4]
- console.log(nums);
 
   let suffix = 1;
-
   for (let i = n - 1; i >= 0; i--) {
-    answer[i] *= suffix;
+    ans[i] *= suffix;
     suffix *= nums[i];
   }
-
-  return answer;
+  return ans;
 }
-
 console.log(productExceptSelf([1, 2, 3, 4]));
-
-
-
-[1, 2, 3, 4]
-
-[1,1,1,1]
-
-[24, 12, 8, 6]
